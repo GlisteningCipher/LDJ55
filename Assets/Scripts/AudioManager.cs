68c7c04ae07db8ac55c802c12bfc660cbc7a2c33
Rene <rene.j.schwartz@gmail.com>
2024-04-14 15:14:28 +0300

    audiomanager methods + menu sounds & juice

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     // Music
+    // Piper Morgan is for main menu
     [SerializeField] private AudioSource _musicHatricia, _musicPiperMorgan;
     // Sfx by category
     [SerializeField]
@@ -13,6 +15,62 @@ public class AudioManager : MonoBehaviour
     _snare, _cymbal, _handGrab,
     _magicSpell2,
     _uiMenuBack, _uiPause, _uiSelect, _uiStartGame;
+
+    public static AudioManager Instance;
+
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Start()
+    {
+        MusicMainMenuPlay();
+    }
+
+    //* MUSIC
+
+    public void MusicMainMenuPlay()
+    {
+        _musicPiperMorgan.Play();
+        _musicHatricia.Stop();
+    }
+
+    /// <summary> Can also use just the other music pieces play method.</summary>
+    public void MusicMainMenuStop() => _musicPiperMorgan.Play();
+
+    public void MusicGameplayPlay()
+    {
+        _musicPiperMorgan.Stop();
+        _musicHatricia.Play();
+    }
+
+    /// <summary> Can also use just the other music pieces play method.</summary>
+    public void MusicGameplayStop() => _musicHatricia.Stop();
+
+    //* SOUND EFFECTS
+
+    public void SfxCrowdApplause() => _crowdApplause.Play();
+    public void SfxCrowdBoo() => _crowdBoo.Play();
+
+    public void SfxObjectPickUp() => _pickUpObject.Play();
+    public void SfxObjectDrop() => _dropObject.Play();
+
+    public void SfxSnare() => _snare.Play();
+    public void SfxCymbal() => _cymbal.Play();
+    public void SfxHandGrab() => _handGrab.Play();
+
+    public void SfxMagicSpell2() => _magicSpell2.Play();
+
+    public void SfxUiMenuBack() => _uiMenuBack.Play();
+    public void SfxUiPause() => _uiPause.Play();
+    public void SfxUiSelect() => _uiSelect.Play();
+    public void SfxUiStartGame() => _uiStartGame.Play();
+
+
+
+
 
 
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 /// <summary> Provides methods for the main menu buttons and other things in main menu if needed. 
@@ -13,6 +15,7 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject _dimmerPanel, _dimmerPanelBlockingPause;
     [SerializeField] private GameObject _creditsContainer;
     [SerializeField] private GameObject _pauseContainer;
+    [SerializeField] private float _uiPanelOpenTime = 0.5f, _uiPanelCloseTime = 0.3f;
 
     public enum MenuState { Paused, Playing }
     public MenuState State = MenuState.Playing;
@@ -31,6 +34,7 @@ public class MainMenu : MonoBehaviour
         State = MenuState.Paused;
         _pauseContainer.SetActive(true);
         _dimmerPanel.SetActive(true);
+        AudioManager.Instance.SfxUiPause();
     }
 
     public void BUTTON_ResumeClosePauseMenu()
@@ -38,12 +42,18 @@ public class MainMenu : MonoBehaviour
         State = MenuState.Playing;
         _pauseContainer.SetActive(false);
         _dimmerPanel.SetActive(false);
+        AudioManager.Instance.SfxUiMenuBack();
     }
 
     public void BUTTON_StartGame()
     {
-        _mainMenuContainer.SetActive(false);
-        // todo Game Manager / Scene loader Show Level 1
+        AudioManager.Instance.SfxUiStartGame();
+        DOVirtual.DelayedCall(0.5f, () =>
+        {
+            _mainMenuContainer.SetActive(false);
+            // todo Game Manager / Scene loader Show Level 1
+            AudioManager.Instance.MusicGameplayPlay();
+        });
     }
 
     public void BUTTON_MainMenuOpen()
@@ -58,8 +68,11 @@ public class MainMenu : MonoBehaviour
 
     public void BUTTON_SettingsOpen()
     {
+        AudioManager.Instance.SfxUiSelect();
+
         // todo Stop game speed in case started from game view
         _settingsContainer.SetActive(true);
+        _settingsContainer.transform.DOScale(1, _uiPanelOpenTime).From(Vector3.zero);
 
         if (State == MenuState.Paused)
         {
@@ -75,25 +88,31 @@ public class MainMenu : MonoBehaviour
 
     public void BUTTON_SettingsClose()
     {
-        _settingsContainer.SetActive(false);
+        AudioManager.Instance.SfxUiMenuBack();
 
-        if (State == MenuState.Paused)
+        _settingsContainer.transform.DOScale(0, _uiPanelCloseTime).OnComplete(() =>
         {
-            // Going back to paused
-            _dimmerPanelBlockingPause.SetActive(false);
-            _dimmerPanel.SetActive(true);
-        }
-        else
-        {
-            // Going back to plain old main menu
-            _dimmerPanel.SetActive(false);
-            _dimmerPanelBlockingPause.SetActive(false);
-        }
+            _settingsContainer.SetActive(false);
+
+            if (State == MenuState.Paused)
+            {
+                // Going back to paused
+                _dimmerPanelBlockingPause.SetActive(false);
+                _dimmerPanel.SetActive(true);
+            }
+            else
+            {
+                // Going back to plain old main menu
+                _dimmerPanel.SetActive(false);
+                _dimmerPanelBlockingPause.SetActive(false);
+            }
+        });
     }
 
     public void BUTTON_CreditsOpen()
     {
         _creditsContainer.SetActive(true);
+        _creditsContainer.transform.DOScale(1, _uiPanelOpenTime).From(Vector3.zero);
 
         if (State == MenuState.Paused)
         {
@@ -105,32 +124,48 @@ public class MainMenu : MonoBehaviour
             _dimmerPanel.SetActive(true);
             _dimmerPanelBlockingPause.SetActive(false);
         }
+
+        AudioManager.Instance.SfxUiSelect();
     }
 
     public void BUTTON_CreditsClose()
     {
-        _creditsContainer.SetActive(false);
+        AudioManager.Instance.SfxUiMenuBack();
 
-        if (State == MenuState.Paused)
+        _creditsContainer.transform.DOScale(0, _uiPanelCloseTime).OnComplete(() =>
         {
-            // Going back to paused
-            _dimmerPanelBlockingPause.SetActive(false);
-            _dimmerPanel.SetActive(true);
-        }
-        else
-        {
-            // Going back to plain old main menu
-            _dimmerPanel.SetActive(false);
-            _dimmerPanelBlockingPause.SetActive(false);
-        }
+            _creditsContainer.SetActive(false);
+
+            if (State == MenuState.Paused)
+            {
+                // Going back to paused
+                _dimmerPanelBlockingPause.SetActive(false);
+                _dimmerPanel.SetActive(true);
+            }
+            else
+            {
+                // Going back to plain old main menu
+                _dimmerPanel.SetActive(false);
+                _dimmerPanelBlockingPause.SetActive(false);
+            }
+        });
     }
 
     public void BUTTON_QuitGame()
     {
+        AudioManager.Instance.SfxUiMenuBack();
+        foreach (var item in GameObject.FindObjectsOfType<Button>())
+        {
+            item.transform.DOScale(0, 0.4f);
+        }
+
+        DOVirtual.DelayedCall(0.5f, () =>
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #else
-        Application.Quit();
+            Application.Quit();
 #endif
+        });
     }
 }
