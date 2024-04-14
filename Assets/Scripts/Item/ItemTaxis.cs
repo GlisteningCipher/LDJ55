42d65486aea35b76c4ad31d2e661beaccfc87bf0
Justin Law <jlawhome08@gmail.com>
2024-04-13 20:18:39 -0700

    Item Follow and Avoid Behavior
    items can now pursue and avoid other objects

---
+++ Assets/Scripts/Item/ItemTaxis.cs
@@ -0,0 +1,55 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTaxis : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] bool targetPlayer = false;
+    [SerializeField] float lerpSpeed = 0.1f;
+    [SerializeField] float maxSpeed = 0f;
+    [SerializeField] float detectionRadius = 5f;
+    [SerializeField] bool avoidTarget = false;
+    [SerializeField] Bounds bounds;
+
+    private void Awake()
+    {
+        var item = GetComponent<Item>();
+        item.OnCarry.AddListener(() => enabled = false);
+        item.OnDrop.AddListener(() => enabled = true);
+    }
+
+    private void Start()
+    {
+        if(targetPlayer) target = Familiar.Instance.transform;
+    }
+
+    public void FixedUpdate()
+    {
+        if (!target) return;
+
+        var difference = target.position - transform.position;
+        if (difference.magnitude > detectionRadius) return;
+
+        if (!avoidTarget)
+        {
+            var displacement = Vector3.Lerp(Vector3.zero, difference, lerpSpeed);
+            if (displacement.magnitude > maxSpeed) displacement = displacement.normalized * maxSpeed;
+            transform.position += new Vector3(displacement.x, displacement.y, 0);
+        }
+        else
+        {
+            difference *= -1;
+            var distance = difference.magnitude;
+            var maxMovement = difference.normalized * maxSpeed;
+            var displacement = Vector3.Lerp(Vector3.zero, maxMovement, 1-distance/detectionRadius);
+            transform.position += new Vector3(displacement.x, displacement.y, 0);
+        }
+
+        var closestPoint = (Vector2)bounds.ClosestPoint((Vector2)transform.position);
+        if ((Vector2)transform.position != closestPoint)
+        {
+            transform.position = closestPoint;
+        }
+    }
+}
