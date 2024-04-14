3de69cb2d83e61bf1ed5eb241b74c1606bb33a44
Rene <rene.j.schwartz@gmail.com>
2024-04-14 03:02:16 +0300

    Main Menu WIP
    - added overlays, button methods and prefabs

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary> Provides methods for the main menu buttons and other things in main menu if needed. 
+/// Menu button methods call other more suitable scripts if needed. </summary>
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainMenuContainer;
+    [SerializeField] private GameObject _settingsContainer;
+    [SerializeField] private GameObject _dimmerPanel;
+    [SerializeField] private GameObject _creditsContainer;
+
+    public void Start()
+    {
+        _mainMenuContainer.SetActive(true);
+        _settingsContainer.SetActive(false);
+        _dimmerPanel.SetActive(false);
+        _creditsContainer.SetActive(false);
+    }
+
+    public void BUTTON_StartGame()
+    {
+        if (_mainMenuContainer)
+            _mainMenuContainer.SetActive(false);
+
+        // todo Game Manager / Scene loader Show Level 1
+    }
+
+    public void BUTTON_SettingsOpen()
+    {
+        // todo Stop game speed in case started from game view
+
+        _dimmerPanel.SetActive(true);
+        _settingsContainer.SetActive(true);
+    }
+
+    public void BUTTON_SettingsClose()
+    {
+        // todo Start game speed in case started from game view
+
+        _dimmerPanel.SetActive(false);
+        _settingsContainer.SetActive(false);
+    }
+
+    public void BUTTON_CreditsOpen()
+    {
+        // todo Stop game speed in case started from game view
+        _dimmerPanel.SetActive(true);
+        _creditsContainer.SetActive(true);
+    }
+
+    public void BUTTON_CreditsClose()
+    {
+        // todo Stop game speed in case started from game view
+        _dimmerPanel.SetActive(false);
+        _creditsContainer.SetActive(false);
+    }
+
+    public void BUTTON_QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
