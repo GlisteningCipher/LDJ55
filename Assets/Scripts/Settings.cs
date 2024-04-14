82c1841c8be1af00b0d30f5ac8ea013b3d0b892d
Rene <rene.j.schwartz@gmail.com>
2024-04-14 12:49:14 +0300

    audiomixer and vol settings code WIP

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _masterVolSlider, _musicVolSlider, _sfxVolSlider;
+
+
+    void Start()
+    {
+        SetSoundLevelsBasedOnSliders(); // should be around 50 % of volume at start
+    }
+
+    private void SetSoundLevelsBasedOnSliders()
+    {
+        AudioSetMasterVol(_masterVolSlider.value);
+        AudioSetMusicVol(_musicVolSlider.value);
+        AudioSetSfxVol(_sfxVolSlider.value);
+    }
+
+    public void AudioSetMasterVol(float value)
+    {
+        _audioMixer.SetFloat("VolumeOfMaster", value);
+    }
+
+    public void AudioSetMusicVol(float value)
+    {
+        _audioMixer.SetFloat("VolumeOfMusic", value);
+    }
+
+    public void AudioSetSfxVol(float value)
+    {
+        _audioMixer.SetFloat("VolumeOfSfx", value);
+    }
+
+}
