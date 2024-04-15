c3637a833f30534723aebc12d74ffd038cbcd75c
Rene <rene.j.schwartz@gmail.com>
2024-04-15 13:14:58 +0300

    AudioManager new sounds

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,14 +7,16 @@ public class AudioManager : MonoBehaviour
 {
     // Music
     // Piper Morgan is for main menu
-    [SerializeField] private AudioSource _musicHatricia, _musicPiperMorgan;
+    [SerializeField] private AudioSource _musicHatricia, _musicPiperMorgan, _musicMenu, _musicGameplay;
     // Sfx by category
     [SerializeField]
     private AudioSource _crowdBoo, _crowdApplause,
     _pickUpObject, _dropObject,
-    _snare, _cymbal, _handGrab,
+    _snare, _cymbal, _handGrab, _handIncoming, _badComponent, _goodComponent, _nextLevel,
     _magicSpell1, _magicSpell2,
-    _uiMenuBack, _uiPause, _uiSelect, _uiStartGame;
+    _failure, _victory,
+    _uiMenuBack, _uiPause, _uiSelect, _uiStartGame,
+    _uiPaperButtonPress, _uiPaperGoingBack, _uiPaperButtonHover;
 
     public static AudioManager Instance;
 
@@ -60,6 +62,17 @@ public class AudioManager : MonoBehaviour
     public void SfxSnare() => _snare.Play();
     public void SfxCymbal() => _cymbal.Play();
     public void SfxHandGrab() => _handGrab.Play();
+    public void SfxHandIncoming() => _handIncoming.Play();
+    public void SfxBadComponent() => _badComponent.Play();
+    public void SfxGoodComponent() => _goodComponent.Play();
+    public void SfxNextLevel() => _nextLevel.Play();
+
+    public void SfxFailure() => _failure.Play();
+    public void SfxVictory() => _victory.Play();
+
+    public void SfxPaperHover() => _uiPaperButtonHover.Play();
+    public void SfxPaperPressButton() => _uiPaperButtonPress.Play();
+    public void SfxPaperGoBack() => _uiPaperGoingBack.Play();
 
     public void SfxMagicSpell1() => _magicSpell1.Play();
     public void SfxMagicSpell2() => _magicSpell2.Play();
